99f5136a8541f3cab566c6e0b65763053eb4f9fc
Saif <147185465+Innocent-X@users.noreply.github.com>
2023-11-28 21:43:34 +0500

    Update Determinant.c

--- Determinant.c
+++ Determinant.c
@@ -1,6 +1,68 @@
+/*Program to find determinant  of am matrix 
+  Author: Saifullah
+*/
+
+//header files 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+//function prototype
+void print_Matrix(double **matrix, int rows, int columns);//function for printing matrix
+double determinant(double **matrix, int rows, int columns);//function to fond determinant
+
+
+int main(void){
+    //declairing variable
+    int order;
+    //taking input
+    printf("Enter the order of square matrix which determinant to be found: ");
+    scanf("%d", &order);
+
+    
+    
+
+    double **matrix;//creating 2 D array 
+
+    matrix = (double **)malloc(order * (sizeof(double *)));//allocating memory for rows using dynamic memory allocation
+    for (int i = 0; i < order; i++)
+    {
+        matrix[i] = (double *)malloc(order * sizeof(double));//allocating memory for col using dynamic memory allocation
+    }
+
+    //taking input of matrix
+    for (int i = 0; i < order; i++)
+    {
+        for (int j = 0; j < order; j++)
+        {
+            printf("Enter the element of row %d and col %d : ", i, j);
+            scanf("%lf", &matrix[i][j]);
+        }
+    }
+   
+    //printing matrix which user enter
+    printf("%s\n","Matrix you enter:");
+    print_Matrix(matrix,order,order);
+
+    if (order==1)//if order of matrix is 1 
+    {
+        printf("Determinant of matrix is %.2lf",matrix[0][0]);
+        return 0;
+    }
+    
+    printf("Determinant of matrix is  %.2lf\n",determinant(matrix, order, order));//printing determiant of matrix
+    
+
+    //freeing memory which we allocated 
+    for (int i = 0; i < order; i++)
+    {
+        free(matrix[i]);
+    }
+    free(matrix);
+    return 0;
+
+    
+}//end main
+//function defination
 void print_Matrix(double **matrix, int rows, int columns)
 {
 
@@ -10,17 +72,17 @@ void print_Matrix(double **matrix, int rows, int columns)
         {
             printf("%.1f\t", matrix[i][j]);
         }
-        printf("\n");
+        puts("");
     }
 }
 double determinant(double **matrix, int rows, int columns)
 {
     double determinant = 1;
-    int row = 0;
+    int current_row = 0;
 
     for (int col = 0; col < columns; col++)
     {
-        int pivot_row = row; // taking 1st row as pivot
+        int pivot_row =current_row; // taking 1st row as pivot
         // finding row which have least pivot
         while (pivot_row < rows && matrix[pivot_row][col] == 0)
         {
@@ -32,99 +94,49 @@ double determinant(double **matrix, int rows, int columns)
             continue;
         }
 
-        // pivot is not in first row dont have least pivot change it
-        if (pivot_row != row)
+        // pivot is not in current row  change it
+        if (pivot_row != current_row)
         {
-            determinant *= -1;//as we swapp we change sign of determinant
+            determinant *= -1; // as we swap we change sign of determinant
             for (int i = 0; i <= columns; i++)
             {
                 double temp = matrix[pivot_row][i];
-                matrix[pivot_row][i] = matrix[row][i];
-                matrix[row][i] = temp;
+                matrix[pivot_row][i] = matrix[current_row][i];
+                matrix[current_row][i] = temp;
             }
         }
 
-        // divding pivot row with leading entery
-        double pivot_element = matrix[row][col];
+        // mutilpying determiant with leading entery
+        double pivot_element = matrix[current_row][col];
         determinant *= pivot_element;
-        for (int i = 0; i < columns; i++)
-        {
-            matrix[row][i] /= pivot_element;
-        }
 
-        // maaking zeros below leading entery of pivot row
-        for (int i = row + 1; i < rows; i++)
+        // making zeros below leading entery of pivot row
+        for (int i = current_row + 1; i < rows; i++)
         {
-            double factor_element = matrix[i][col];
+            double factor_element = matrix[i][col] / pivot_element;
 
             for (int j = 0; j < columns; j++)
             {
-                matrix[i][j] -= factor_element * matrix[row][j];
+                matrix[i][j] -= factor_element * matrix[current_row][j];
             }
         }
 
-        row++;
+        current_row++;
     }
-    bool fullRank = false;
+    bool fullRank = false; // checking if ref is full rank
     for (int i = 0; i < columns; i++)
     {
         double leading_entry = matrix[columns - 1][i];
         if (leading_entry != 0)
         {
             fullRank = true;
-
             break;
         }
     }
-    if (fullRank == false)//if matrix is not full rank determianant is 0
+    if (fullRank == false) // if matrix is not full rank determianant is 0
     {
         return 0;
     }
 
     return determinant;
-}
-int main() {
-  
-    int rows = 0;
-    int col = 0;
-    printf("Enter the number of rows : ");
-    scanf("%d", &rows);
-
-    printf("Enter the number of columns : ");
-    scanf("%d", &col);
-     if (rows!=col)
-    {
-        puts("Inverse can not be calculated.Matrix should be square.");
-        return 0;
-    }
-
-    double **matrix; 
-
-    matrix = (double **)malloc(rows * (sizeof(double *)));
-    for (int i = 0; i < rows; i++)
-    {
-        matrix[i] = (double *)malloc(col * sizeof(double));
-    }
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            printf("Enter the element of row %d and col %d : ", i, j);
-            scanf("%lf", &matrix[i][j]);
-        }
-    }
-    printf("%s\n","Matrix you enter:");
-    print_Matrix(matrix,rows,col);
-    printf("Determinant of matrix is  %.2lf\n",determinant(matrix, rows, col));
-
-    
-    for (int i = 0; i < rows; i++)
-    {
-        free(matrix[i]);
-    }
-    free(matrix);
-    return 0;
-
-    
 }
