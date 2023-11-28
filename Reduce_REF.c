6a3c0e4712b0d23f2e2c6a15bd375fefdf72bdcd
Saif <147185465+Innocent-X@users.noreply.github.com>
2023-11-28 21:46:07 +0500

    Update Reduce_REF.c

--- Reduce_REF.c
+++ Reduce_REF.c
@@ -1,6 +1,59 @@
+/*Program to reduce row echelon form  of a matrix
+  Author: Saifullah
+*/
+//header files 
 #include <stdio.h>
 #include <stdlib.h>
+void print_Matrix(double **matrix, int rows, int columns);
+void convert_inRef(double **matrix, int rows, int columns);
+void convert_into_reduce(double **matrix, int rows, int columns);
 
+int main()
+{
+    //declairing variable
+    int rows,column;
+
+    //taking input
+    printf("Enter the number of rows : ");
+    scanf("%d", &rows);
+
+    printf("Enter the number of columns : ");
+    scanf("%d", &column);
+
+    double **matrix;//creating 2-D array using pointer
+
+    matrix = (double **)malloc(rows * (sizeof(double *)));
+    for (int i = 0; i < rows; i++)
+    {
+        matrix[i] = (double *)malloc(column * sizeof(double));
+    }
+    
+    //taking input elements of array
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            printf("Enter the element of row %d and col %d : ", i, j);
+            scanf("%lf", &matrix[i][j]);
+        }
+    }
+    //printing matrix
+    printf("%s\n", "Matrix you enter:");
+    print_Matrix(matrix, rows, column);
+    //caling function 
+    convert_into_reduce(matrix, rows, column);
+    //printing rref
+    printf("%s\n", "Matrix reduce row echelon form:");
+    print_Matrix(matrix, rows, column);
+
+    //freeing memory
+      for (int i = 0; i < rows; i++)
+    {
+        free(matrix[i]);
+    }
+    free(matrix);
+    return 0;
+}
 void print_Matrix(double **matrix, int rows, int columns)
 {
 
@@ -10,16 +63,16 @@ void print_Matrix(double **matrix, int rows, int columns)
         {
             printf("%.1f\t", matrix[i][j]);
         }
-        printf("\n");
+        puts("");
     }
 }
-void **convert_into_ref(double **matrix, int rows, int columns)
+void convert_inRef(double **matrix, int rows, int columns)
 {
-    int row = 0;
+    int current_row = 0;//the raw on which operation to be applied
 
     for (int col = 0; col < columns; col++)
     {
-        int pivot_row = row; // taking 1st row as pivot
+        int pivot_row = current_row; // taking 1st row as pivot
         // finding row which have least pivot
         while (pivot_row < rows && matrix[pivot_row][col] == 0)
         {
@@ -31,37 +84,37 @@ void **convert_into_ref(double **matrix, int rows, int columns)
             continue;
         }
 
-        // pivot is not in first row dont have least pivot change it
-        if (pivot_row != row)
+        // pivot is not in current row dont have least pivot change it
+        if (pivot_row != current_row)
         {
             for (int i = 0; i <= columns; i++)
             {
                 double temp = matrix[pivot_row][i];
-                matrix[pivot_row][i] = matrix[row][i];
-                matrix[row][i] = temp;
+                matrix[pivot_row][i] = matrix[current_row][i];
+                matrix[current_row][i] = temp;
             }
         }
 
-        double pivot_element = matrix[row][col];
+        double pivot_element = matrix[current_row][col];
 
         // maaking zeros below leading entery of pivot row
-        for (int i = row + 1; i < rows; i++)
+        for (int i = current_row + 1; i < rows; i++)
         {
             double factor_element = matrix[i][col];
 
             for (int j = 0; j < columns; j++)
             {
-                matrix[i][j] -= factor_element * matrix[row][j] / pivot_element;
+                matrix[i][j] -= factor_element * matrix[current_row][j] / pivot_element;
             }
         }
 
-        row++;
+        current_row++;
     }
 }
-void **convert_into_reduce(double **matrix, int rows, int columns)
+void convert_into_reduce(double **matrix, int rows, int columns)
 {
-    matrix = convert_into_ref(matrix, rows, columns);                // converting matrix into ref
-    for (int current_row = rows - 1; current_row > 0; current_row--) // for traversing through rows in backward
+    convert_inRef(matrix, rows, columns);                // converting matrix into ref
+    for (int current_row = rows - 1; current_row >=0; current_row--) // for traversing through rows in backward
     {
         int pivot_column = -1;
         for (int j = 0; j < columns; j++)
@@ -72,7 +125,7 @@ void **convert_into_reduce(double **matrix, int rows, int columns)
                 break;
             }
         }
-        if (pivot_column == -1)//if row dont have pivot i.e zero row
+        if (pivot_column == -1)//if row dont have pivot i.e zero row goes to above row
         {
             continue;
         }
@@ -83,10 +136,15 @@ void **convert_into_reduce(double **matrix, int rows, int columns)
             {
                 matrix[current_row][i] /= leading_entry;
             }
+            if (current_row==0)//if we at the top row break loop because we dont have make any zero
+            {
+                break;
+            }
+            
 
             for (int i = 0; i < current_row; i++)
             {
-                double element = matrix[i][pivot_column];
+                double element = matrix[i][pivot_column];//element that we want to convert to 0 
                 for (int j = 0; j < columns; j++)
                 {
                     matrix[i][j] = matrix[i][j] - (element * matrix[current_row][j]);
@@ -94,57 +152,4 @@ void **convert_into_reduce(double **matrix, int rows, int columns)
             }
         }
     }
-    for (int i = 0; i < columns; i++)//making leading entry of first row 1 
-    {
-        double leading_entry = matrix[0][i];
-        if (leading_entry != 0)
-        {
-            for (int j = 0; j < columns; j++)
-            {
-                matrix[0][j] /= leading_entry;
-            }
-            break;
-        }
-    }
-}
-
-int main()
-{
-
-    int rows = 0;
-    int col = 0;
-    printf("Enter the number of rows : ");
-    scanf("%d", &rows);
-
-    printf("Enter the number of columns : ");
-    scanf("%d", &col);
-
-    double **matrix;
-
-    matrix = (double **)malloc(rows * (sizeof(double *)));
-    for (int i = 0; i < rows; i++)
-    {
-        matrix[i] = (double *)malloc(col * sizeof(double));
-    }
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            printf("Enter the element of row %d and col %d : ", i, j);
-            scanf("%lf", &matrix[i][j]);
-        }
-    }
-    printf("%s\n", "Matrix you enter:");
-    print_Matrix(matrix, rows, col);
-    convert_into_reduce(matrix, rows, col);
-    printf("%s\n", "Matrix reduce row echelon form:");
-    print_Matrix(matrix, rows, col);
-
-      for (int i = 0; i < rows; i++)
-    {
-        free(matrix[i]);
-    }
-    free(matrix);
-    return 0;
-}
+  }
